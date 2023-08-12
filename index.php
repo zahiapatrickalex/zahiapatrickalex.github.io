7527e9e415b8464b47b858f1565abcefab2db4ac
zahia patrick alex <zpatrickalex@gmail.com>
2023-08-12 19:18:52 +0000

    premier envoie en ligne

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>cours github</h1>
+<h2>version 2</h2>
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur ullam excepturi accusamus doloribus quam tempore fugit illo? Aliquam aliquid fuga pariatur alias, dignissimos explicabo incidunt, eum nisi qui veritatis temporibus!</p>
+<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. A omnis sunt perspiciatis expedita iste, praesentium esse dolores! Eos amet praesentium, cumque ab, iusto dolore fugiat quas perspiciatis veniam, sit et?Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet minima ipsa et molestiae aperiam, aut voluptates laboriosam ut facilis ipsum expedita totam quaerat pariatur quis deserunt perferendis ullam sint <quae class="lorem"></quae></p>
+    
+</body>
+</html>
